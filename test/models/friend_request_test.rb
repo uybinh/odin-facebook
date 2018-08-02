2a8d01a51cea6421d841467e2f3a6b67c87936d0
Uy Binh <uybinh@gmail.com>
2018-08-02 16:43:21 +0700

    Edit user fixtures

--- test/models/friend_request_test.rb
+++ test/models/friend_request_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class FriendRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @binh = users(:binh)
+    @xuan = users(:xuan)
+  end
+
+  test "can add friends and reverse friends" do
+    @binh.friends << @xuan
+    assert @xuan.all_friends.include? @binh
+  end
 end
