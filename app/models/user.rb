2b192ddd2443ececd89a6b1f1f07687e3cb90833
Uy Binh <uybinh@gmail.com>
2018-08-02 14:54:41 +0700

    Add friend relationship frist draft

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,41 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+
+
+
+  has_many :received_requests,  class_name: "FriendRequest",
+                            foreign_key: "requested_id",
+                            dependent: :destroy
+
   has_many :sent_requests,  class_name: "FriendRequest",
                             foreign_key: "requester_id",
                             dependent: :destroy
-  has_many :received_requests,  class_name: "FriendRequest",
-                                foreign_key: "requested_id",
-                                dependent: :destroy
-  has_many :requesting, through: :sent_requests,
-                        source: :requested
+
   has_many :requesters, through: :received_requests,
                         source: :requester
+  has_many :requesting, through: :sent_requests,
+                        source: :requested
+
+
+  has_many :friendships, -> { where(accepted: true)},
+                            class_name: "FriendRequest",
+                            foreign_key: "requested_id",
+                            dependent: :destroy
+
+  has_many :reversed_friendships, -> { where(accepted: true)},
+                                  class_name: "FriendRequest",
+                                  foreign_key: "requester_id",
+                                  dependent: :destroy
+
+  has_many :friends,  through: :friendships,
+                      source: :requester
+  has_many :reversed_friends, through: :reversed_friendships,
+                              source: :requested
+
+  def all_friends
+    self.friends + self.reversed_friends
+  end
 end
