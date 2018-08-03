0ce6ca1b5f68ddec89828cb2ec9fe800540819a4
Uy Binh <uybinh@gmail.com>
2018-08-03 10:15:15 +0700

    Add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_010058) do
+ActiveRecord::Schema.define(version: 2018_08_03_030931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2018_08_03_010058) do
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,23 @@
 require 'faker'
 
-User.create!(
+User.create(
   email: "binh@gmail.com",
   password: "password",
   password_confirmation: "password"
 )
 
-User.create!(
+User.create(
   email: "xuan@gmail.com",
   password: "password",
   password_confirmation: "password"
 )
 
 20.times do |n|
-  User.create!(
+  User.create(
     email: Faker::Internet.email,
     password: "password",
     password_confirmation: "password"
   )
 end
+
+10.times { Post.create!(content: Faker::Lorem.paragraph(2, true, 4) ) }
