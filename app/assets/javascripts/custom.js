872bb42a6940ba0710144e8fa66316a00d520249
Uy Binh <uybinh@gmail.com>
2018-08-07 14:58:00 +0700

    Add show/hide animation

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -6,7 +6,18 @@ document.addEventListener('turbolinks:load', () => {
       event.preventDefault()
       let id = event.target.dataset.postId
       let comments = document.querySelector(`.comments[data-post-id='${id}'`)
-      comments.classList.toggle('hidden')
+
+      let classes = comments.classList
+      if (classes.contains('show')){
+        classes.remove('show')
+        classes.add('hidden')
+      } else {
+        classes.remove('initial-hidden')
+        classes.remove('hidden')
+        classes.add('show')
+      }
+
+
       if (event.target.innerText == 'Show comments') {
         event.target.innerText = 'Hide comments'
       } else {
