56c1ef8a351067785501a5a886e824da28056849
Uy Binh <uybinh@gmail.com>
2018-08-10 15:51:39 +0700

    Edit seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,5 +34,3 @@ end
   xuan.posts[n].comments.create(content: Faker::Lorem.sentence, author_id: xuan.id)
   xuan.posts[n].comments.create(content: Faker::Lorem.sentence, author_id: xuan.id)
 end
-
-binh.posts.first.image.attach(io: File.open("/Users/uybinh/Pictures/wallpaper/yosemite-2880x1800-5k-4k-wallpaper-8k-winter-snow-forest-osx-apple-3945.jpg"), filename: "mountain.jpg", content_type: "image/jpg")
